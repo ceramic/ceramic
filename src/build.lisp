dd554d383eeab243832435e92f7d5ddea5f21864
Fernando Borretti <eudoxiahp@gmail.com>
2015-06-30 23:11:29 -0300

    Some changes

--- src/build.lisp
+++ src/build.lisp
@@ -7,18 +7,8 @@
   (:documentation "A Buildapp interface."))
 (in-package :ceramic.build)
 
-(defun install-buildapp ()
-  "Install a local copy of Buildapp."
-  (buildapp:build-buildapp (namestring *buildapp-pathname*)))
-
-(defun ensure-buildapp-is-installed ()
-  "Ensure Buildapp is installed."
-  (unless (probe-file *buildapp-pathname*)
-    (install-buildapp)))
-
 (defun build (system-name output-pathname entry-point)
   "Build an executable from a Lisp system."
-  (ensure-buildapp-is-installed)
   (uiop:run-program (format nil "~S --output ~S --entry ~A --load-system ~A"
                             (namestring *buildapp-pathname*)
                             (namestring output-pathname)
--- src/file.lisp
+++ src/file.lisp
@@ -2,31 +2,20 @@
 (defpackage ceramic.file
   (:use :cl)
   (:export :*ceramic-directory*
-           :*bundler-directory*
-           :*buildapp-pathname*)
-  (:documentation "Ceramic's files and directories."))
+           :*buildapp-pathname*
+           :wipe-data)
+  (:documentation "Ceramic's files and directory utilities."))
 (in-package :ceramic.file)
 
 (defvar *ceramic-directory*
   (merge-pathnames #p".ceramic"
                    (user-homedir-pathname))
-  "The directory where Ceramic stores bundler and user data.")
-
-(defvar *bundler-directory*
-  (merge-pathnames #p"bundler/"
-                   *ceramic-directory*)
-  "The directory where Ceramic stores files related to the bundler.")
+  "The directory where Ceramic stores its files.")
 
 (defvar *buildapp-pathname*
-  (merge-pathnames #p"buildapp"
-                   *bundler-directory*)
+  (merge-pathnames #p"buildapp" *ceramic-directory*)
   "The pathname to the local copy of Buildapp.")
 
-(defun ensure-ceramic-directory ()
-  "Ensure the Ceramic directory exists."
-  (ensure-directories-exist *ceramic-directory*))
-
-(defun ensure-bundler-directory ()
-  "Ensure the bundler directory exists."
-  (ensure-ceramic-directory)
-  (ensure-directories-exist *bundler-directory*))
+(defun wipe-data ()
+  "Wipe all Ceramic related data."
+  (uiop:delete-directory-tree *ceramic-directory* :validate t))
