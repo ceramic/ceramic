e8d9fcda9d6ecd490fe64d4e04f30ffe63531e43
Fernando Borretti <eudoxiahp@gmail.com>
2015-07-03 00:26:12 -0300

    Add some docs

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:ceramic)
+ :documents ((:title "Ceramic"
+              :authors ("Fernando Borretti")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("manual.scr"))))
