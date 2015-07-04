26ba91bcea1dc32653fc94c93e356e3e364620c3
Fernando Borretti <eudoxiahp@gmail.com>
2015-07-03 23:26:33 -0300

    Implement shelling out to buildapp

--- src/build.lisp
+++ src/build.lisp
@@ -2,15 +2,30 @@
 (defpackage ceramic.build
   (:use :cl)
   (:import-from :ceramic.file
+                :*ceramic-directory*
                 :*buildapp-pathname*)
   (:export :build)
   (:documentation "A Buildapp interface."))
 (in-package :ceramic.build)
 
+(defmacro with-manifest-file ((pathname) &body body)
+  "Create an ASDF manifest file and delete it when we're done."
+  `(progn
+     (ql:write-asdf-manifest-file ,pathname)
+     (unwind-protect
+          (progn ,@body)
+       (delete-file ,pathname))))
+
 (defun build (system-name output-pathname entry-point)
   "Build an executable from a Lisp system."
-  (uiop:run-program (format nil "~S --output ~S --entry ~A --load-system ~A"
-                            (namestring *buildapp-pathname*)
-                            (namestring output-pathname)
-                            entry-point
-                            system-name)))
+  (let ((manifest-pathname (merge-pathnames #p"manifest.txt"
+                                            *ceramic-directory*)))
+    (with-manifest-file (manifest-pathname)
+      (uiop:run-program
+       (format nil
+               "~S --manifest-file ~S --output ~S --entry ~A --load-system ~A"
+               (namestring *buildapp-pathname*)
+               (namestring manifest-pathname)
+               (namestring output-pathname)
+               entry-point
+               (string-downcase (symbol-name system-name)))))))
