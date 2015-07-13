ce2da92778f1b7a1a975d7253e9ead77da66bc70
Fernando Borretti <eudoxiahp@gmail.com>
2015-07-12 21:42:57 -0300

    Change the way tests are run

--- t/ceramic.lisp
+++ t/ceramic.lisp
@@ -4,18 +4,20 @@
   (:import-from :ceramic.file
                 :*ceramic-directory*
                 :*buildapp-pathname*
-                :wipe-data))
+                :wipe-data)
+  (:export :run-tests))
 (in-package :ceramic-test)
 
-(let* ((*ceramic-directory* (asdf:system-relative-pathname :ceramic-test
-                                                           #p"t/ceramic/"))
-       (*buildapp-pathname* (merge-pathnames #p"buildapp"
-                                             *ceramic-directory*)))
-  (run! 'ceramic-test.electron.tools:tests)
-  (run! 'ceramic-test.electron:tests)
-  (print *ceramic-directory*)
-  (print (ceramic.electron:release-directory))
-  (run! 'ceramic-test.setup:tests)
-  (run! 'ceramic-test.integration:tests)
-  ;; Cleanup
-  (uiop:delete-directory-tree *ceramic-directory* :validate t))
+(defun run-tests ()
+  (let* ((*ceramic-directory* (asdf:system-relative-pathname :ceramic-test
+                                                             #p"t/ceramic/"))
+         (*buildapp-pathname* (merge-pathnames #p"buildapp"
+                                               *ceramic-directory*)))
+    (run! 'ceramic-test.electron.tools:tests)
+    (run! 'ceramic-test.electron:tests)
+    (print *ceramic-directory*)
+    (print (ceramic.electron:release-directory))
+    (run! 'ceramic-test.setup:tests)
+    (run! 'ceramic-test.integration:tests)
+    ;; Cleanup
+    (uiop:delete-directory-tree *ceramic-directory* :validate t)))
