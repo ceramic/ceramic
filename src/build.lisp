651813533187bef01ae4479c84070261492a3302
Fernando Borretti <eudoxiahp@gmail.com>
2015-07-12 22:00:02 -0300

    Remove an unnecessary macro

--- src/build.lisp
+++ src/build.lisp
@@ -10,14 +10,6 @@
   (:documentation "A Buildapp interface."))
 (in-package :ceramic.build)
 
-(defmacro with-manifest-file ((pathname) &body body)
-  "Create an ASDF manifest file and delete it when we're done."
-  `(progn
-     (ql:write-asdf-manifest-file ,pathname)
-     (unwind-protect
-          (progn ,@body)
-       (delete-file ,pathname))))
-
 (defparameter +command-format+
   "~S --eval ~S --manifest-file ~S --load-system ~A --output ~S --entry ~A")
 
@@ -33,14 +25,16 @@
                           (string-downcase (symbol-name system-name))
                           (namestring output-pathname)
                           entry-point)))
-    (with-manifest-file (manifest-pathname)
-      (multiple-value-bind (output stderr status-code)
-          (uiop:run-program command
-                            :output :string
-                            :error-output :output
-                            :ignore-error-status t)
-        (declare (ignore stderr))
-        (when (not (= status-code 0))
-          (error 'compilation-error
-                 :command command
-                 :message output))))))
+    (ql:write-asdf-manifest-file manifest-pathname)
+    (unwind-protect
+         (multiple-value-bind (output stderr status-code)
+             (uiop:run-program command
+                               :output :string
+                               :error-output :output
+                               :ignore-error-status t)
+           (declare (ignore stderr))
+           (when (not (= status-code 0))
+             (error 'compilation-error
+                    :command command
+                    :message output)))
+      (delete-file manifest-pathname))))
