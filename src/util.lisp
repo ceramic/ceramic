a83969373db2c03e75902c86cf267b46cfec6353
Fernando Borretti <eudoxiahp@gmail.com>
2015-10-12 14:31:00 -0300

    Replace subtract-pathname with uiop:subpathp

--- src/util.lisp
+++ src/util.lisp
@@ -18,15 +18,7 @@
   (assert (uiop:absolute-pathname-p root))
   (assert (uiop:absolute-pathname-p pathname))
   (assert (uiop:subpathp pathname root))
-  (let ((position (mismatch (pathname-directory root)
-                            (pathname-directory pathname)
-                            :test #'equalp)))
-    (make-pathname :directory (if (null position)
-                                  nil
-                                  (cons :relative
-                                        (subseq (pathname-directory pathname)
-                                                position)))
-                   :defaults pathname)))
+  (uiop:subpathp pathname root))
 
 (defun copy-directory (source destination)
   "Copy everything under source to destination."
