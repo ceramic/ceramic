5246328b415630fcd420a2e002d3ec47442362ef
Fernando Borretti <eudoxiahp@gmail.com>
2015-11-04 21:48:56 -0300

    Remove references to Buildapp

--- src/file.lisp
+++ src/file.lisp
@@ -2,7 +2,6 @@
 (defpackage ceramic.file
   (:use :cl)
   (:export :*ceramic-directory*
-           :*buildapp-pathname*
            :wipe-data)
   (:documentation "Ceramic's files and directory utilities."))
 (in-package :ceramic.file)
@@ -11,10 +10,6 @@
   (merge-pathnames #p".ceramic/"
                    (user-homedir-pathname))
   "The directory where Ceramic stores its files.")
-
-(defvar *buildapp-pathname*
-  (merge-pathnames #p"buildapp" *ceramic-directory*)
-  "The pathname to the local copy of Buildapp.")
 
 (defun wipe-data ()
   "Wipe all Ceramic related data."
