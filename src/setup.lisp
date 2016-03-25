6b1eae3d7246819a62d629a30ede4086d679bb1a
Fernando Borretti <eudoxiahp@gmail.com>
2016-03-25 16:28:11 -0300

    Add setup package

--- src/setup.lisp
+++ src/setup.lisp
@@ -1,19 +1,5 @@
 (in-package :cl-user)
 (defpackage ceramic.setup
   (:use :cl)
-  (:import-from :ceramic.log
-                :log-message)
-  (:import-from :ceramic.file
-                :*ceramic-directory*)
-  (:import-from :ceramic.os
-                :*operating-system*
-                :*architecture*)
-  (:export :setup)
-  (:documentation "Set everything up for Ceramic."))
+  (:documentation "Set up everything needed to develop with Ceramic."))
 (in-package :ceramic.setup)
-
-(defun setup ()
-  (log-message "Creating Ceramic directories...")
-  (ensure-directories-exist *ceramic-directory*)
-  (log-message "Downloading a copy of Electron...")
-  (ceramic.electron:setup))
