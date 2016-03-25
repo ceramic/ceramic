e1f156b4e43791c207a1ba61664b68d891bdeff7
Fernando Borretti <eudoxiahp@gmail.com>
2016-03-25 18:32:52 -0300

    Remove most of the package definition

--- src/ceramic.lisp
+++ src/ceramic.lisp
@@ -1,48 +1,6 @@
 (in-package :cl-user)
 (defpackage ceramic
   (:use :cl)
-  (:import-from :ceramic.os
-                :*operating-system*)
-  (:import-from :ceramic.runtime
-                :*releasep*
-                :executable-relative-pathname)
-  (:import-from :ceramic.electron
-                :start-process)
-  (:import-from :electron-tools
-                :binary-pathname)
-  (:import-from :ceramic.file
-                :release-directory)
-  ;; Main interface
-  (:export :setup
-           :interactive
-           :stop-interactive
-           :with-interactive
-           :define-entry-point
-           :quit)
-  ;; Window & accessors
-  (:export :window
-           :make-window
-           :window-title
-           :window-url
-           :window-x
-           :window-y
-           :window-width
-           :window-height
-           :window-resizable-p)
-  ;; Methods
-  (:export :show-window
-           :hide-window
-           :close-window
-           :destroy-window
-           :send-message
-           :maximize-window
-           :unmaximize-window
-           :minimize-window
-           :unminimize-window
-           :center-window
-           :open-dev-tools)
-  ;; Events
-  (:export :*event-dispatcher*)
   (:documentation "The main interface."))
 (in-package :ceramic)
 
