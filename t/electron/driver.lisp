2ac9a84275b815483db76feaa3154be8d8077be3
Fernando Borretti <eudoxiahp@gmail.com>
2016-03-25 11:05:37 -0300

    Add more tests

--- t/electron/driver.lisp
+++ t/electron/driver.lisp
@@ -18,6 +18,40 @@
   :description "Electron driver tests.")
 (in-suite electron-driver)
 
+(defvar *test-directory*
+  (asdf:system-relative-pathname :ceramic-test #p"t/test/"))
+
+(defun test-download-and-extract (os arch directory)
+  (finishes
+    (electron-tools:get-release directory
+                                :operating-system os
+                                :version "0.28.1"
+                                :architecture arch))
+  (is-true
+   (probe-file (electron-tools:binary-pathname directory
+                                               :operating-system os))))
+
+(defun test-changes (directory os)
+  (finishes
+    (ceramic.electron.tools:prepare-release directory :operating-system os))
+  (is-true
+   (probe-file (merge-pathnames #p"main.js"
+                                (electron-tools:app-directory directory :operating-system os))))
+  (is-true
+   (probe-file (merge-pathnames #p"package.json"
+                                (electron-tools:app-directory directory :operating-system os)))))
+
+(defun test-release (os arch)
+  (let ((directory (merge-pathnames
+                    (make-pathname :directory (list :relative
+                                                    (format nil "~A~A" os arch)))
+                    *test-directory*)))
+    (test-download-and-extract os arch directory)
+    (test-changes directory os)))
+
+(test download-electron
+  (test-release :linux :64))
+
 (test start-app
   (let ((release (merge-pathnames #p"LINUX64/"
                                   *test-directory*)))
