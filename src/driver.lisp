a7a7b79f40bcb15d678e22f3605295be8fd0708c
Fernando Borretti <eudoxiahp@gmail.com>
2016-03-25 17:20:27 -0300

    Some driver code

--- src/driver.lisp
+++ src/driver.lisp
@@ -5,7 +5,21 @@
 (in-package :ceramic.driver)
 
 (defclass driver ()
-  ()
+  ((process :accessor driver-process
+            :documentation "The Electron process.")
+   (context :accessor driver-context
+            :type remote-js:buffered-context
+            :documentation "The remote-js object."))
   (:documentation "The Ceramic driver."))
 
-(defvar *driver* (make-instance 'driver))
+(defvar *driver* (make-instance 'driver)
+  "The global driver object.")
+
+(defmethod initialize-instance :after ((driver driver) &key)
+  (setf (driver-context driver)
+        (remote-js:make-buffered-context :callback
+                                         #'(lambda (message)
+                                             (on-message driver message)))))
+
+(defgeneric on-message (driver message)
+  (:documentation "React to a message from a WebSockets client."))
